25ba0844e7993fd0109722cfd4b78ee754ae5fe9
onrush1995 <moniruzzaman.eng1995@gmail.com>
2018-10-31 21:57:24 +0200

    Added Mongoose model Classes & Saving Instances

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
+require("./models/user");
 require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
 
-require("./routes/authRoutes");
 const app = express();
+
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,10 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
 passport.use(
   new GoogleStrategy(
     {
@@ -9,9 +13,7 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("access token", accessToken);
-      console.log("refresh token", refreshToken);
-      console.log("profile:", profile);
+      new User({ googleId: profile.id }).save();
     }
   )
 );
