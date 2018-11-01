e502aaaa06f4e09a8507e00c27e34e88bc1902f6
onrush1995 <moniruzzaman.eng1995@gmail.com>
2018-11-01 17:43:10 +0200

    Enabling users & Cookies Added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookiesSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
 require("./models/user");
 require("./services/passport");
@@ -7,6 +9,15 @@ require("./services/passport");
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookiesSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKeys]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
