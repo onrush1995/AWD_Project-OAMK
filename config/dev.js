58e47f58b41cff100afeac2146da53a6a0d7cb9d
onrush1995 <moniruzzaman.eng1995@gmail.com>
2018-11-13 09:30:02 +0200

    made a change

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+//dev.js -don't commit this!!!
+module.exports = {
+  googleClientID:
+    "620534927884-nh69444f882pp84pvsg16n5r8up1pido.apps.googleusercontent.com",
+  googleClientSecret: "ozEU87x0n-dKf7aYkYuYuqeE",
+  mongoURI: "mongodb://Monir:M12345@ds247223.mlab.com:47223/emaily-dev",
+  cookieKey: "jddhjfheiuf5tujkjchfjd5srueeijks967fyjdfh"
+};
