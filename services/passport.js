31ee8949d20887b4c52621506880f0eb8c98a57c
onrush1995 <moniruzzaman.eng1995@gmail.com>
2018-11-13 10:10:14 +0200

    Google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -20,8 +20,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
-      proxy:true
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
