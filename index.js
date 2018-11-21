56f907dbc5f3f6c060b53159b82bbe6e9b825c51
onrush1995 <moniruzzaman.eng1995@gmail.com>
2018-11-21 14:25:55 +0200

    Payment Handiling System for Clients Completed Using StripeApi

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookiesSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 const keys = require("./config/keys");
-require("./models/User");
-require("./services/passport");
+const authRoutes = require("./models/User");
+const billingRoutes = require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookiesSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
