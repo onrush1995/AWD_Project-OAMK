140d2e38d5456d1fcd8bc6d348c4fb1b017e4bce
onrush1995 <moniruzzaman.eng1995@gmail.com>
2018-11-26 14:30:56 +0200

    Verifying credits and Setting Up Relationalship Fields

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const cookiesSession = require("cookie-session");
 const passport = require("passport");
 const bodyParser = require("body-parser");
 const keys = require("./config/keys");
-const authRoutes = require("./models/User");
-const billingRoutes = require("./services/passport");
+require("./models/User");
+require("./models/Survey");
+require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
 
@@ -23,6 +24,7 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+require("./routes/surveyRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
   //Express will serve up production assets
